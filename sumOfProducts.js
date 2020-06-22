cdc6235073ece34a82adf62d8d63569d292c2554
LusineGhalechyan <57481746+LusineGhalechyan@users.noreply.github.com>
2020-06-22 14:38:58 +0400

    Update sumOfProducts.js

--- sumOfProducts.js
+++ sumOfProducts.js
@@ -1,18 +1,18 @@
 const sumOfnumbersFromOneToTen = () => {
       let sum = 0;
     for (let i = 1; i <= 10; i++) {
-      sum += i
+      sum += i;
     }  
-      return sum
+      return sum;
   }
 
-let currentTotal = 0
-let mainTotal = 0
+let currentTotal = 0;
+let mainTotal = 0;
 
 for (let i = 1; i <= 10; i++) {
   let resOfSum = sumOfnumbersFromOneToTen();
   currentTotal += resOfSum;
-    mainTotal += currentTotal
+    mainTotal += currentTotal;
 }
 
 console.log(mainTotal)
